fef9627047a1ed35da3e80a2426c245480b8bfd3
Steph B <SMBoatman@live.co.uk>
2022-04-03 14:26:38 +0000

    add start of JS script to JS file

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progresstext = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
