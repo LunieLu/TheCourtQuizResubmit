6175f2676e854d757c77cd303199678eff022df7
Steph B <SMBoatman@live.co.uk>
2022-04-06 19:40:26 +0000

    adjust js code on end & highscores per validation results

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,34 +1,32 @@
-const username = document.querySelector('#username')
-const saveScoreBtn = document.querySelector('#saveScoreBtn')
-const finalScore = document.querySelector('#finalScore')
-const mostRecentScore = localStorage.getItem('mostRecentScore')
+const username = document.querySelector('#username');
+const saveScoreBtn = document.querySelector('#saveScoreBtn');
+const finalScore = document.querySelector('#finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
 
-const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
-const MAX_HIGH_SCORES = 10
-
-finalScore.innerText = mostRecentScore
+finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !username.value
-})
+    saveScoreBtn.disabled = !username.value;
+});
 
-saveHighScore = e => {
-    e.preventDefault()
+const saveHighScore = e => {
+    e.preventDefault();
 
     const score = {
         score: mostRecentScore,
         name: username.value
-    }
+    };
 
-    highScores.push(score)
+    highScores.push(score);
 
     highScores.sort((a,b) => {
-        return b.score - a.score
-    }) 
+        return b.score - a.score;
+    }); 
 
-    highScores.splice(10)
+    highScores.splice(10);
 
-    localStorage.setItem('highScores', JSON.stringify(highScores))
-    window.location.href('/')
-}
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.href('/');
+};
--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,32 +1,7 @@
-const highScoresList = document.querySelector('#highScoresList')
-const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+const highScoresList = document.querySelector('#highScoresList');
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
 highScoresList.innerHTML =
 highScores.map(score => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`
-}).join('')
-
-
-
-
-
-
-let firstName = "Steph";
-let lastName = "Boatman";
-function fullName() {
-    return firstName + lastName;
-}
-
-fullName();
-
-function add(a, b) {
-    let c = a + b;
-    return c;
-}
-add(100, 100);
-
-function multiply(a, b) {
-    let d = a * b;
-}
-
-multiply(c, c);
+    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+}).join('');
