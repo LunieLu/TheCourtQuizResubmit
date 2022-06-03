7286644ae608006cbf4e5e05121df13875ab4783
Steph B <SMBoatman@live.co.uk>
2022-06-03 15:23:02 +0000

    add js functionality to Start Quiz button for mouse hover over effect

--- assets/js/instructions.js
+++ assets/js/instructions.js
@@ -5,3 +5,13 @@ function on() {
 function off() {
     document.getElementById("instructions").style.display = "none";
 }
+
+function start(obj) {
+    obj.style.backgroundColor = "white";
+    obj.style.color = "black";
+    obj.style.border = "1px black";
+}
+
+function nostart(obj) {
+    obj.style = "none";
+}
